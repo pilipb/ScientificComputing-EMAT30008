09ed8e7b83502e3b99db09a8e8f7cc99ac4a4c3f
pilipb <philipblecher@hotmail.com>
2023-02-10 12:46:50 +0000

    deleted old files

---
+++ shooting.py
@@ -0,0 +1,43 @@
+from solve_to import *
+from solvers import *
+
+'''
+shooting method will solve the ODE using root finding method to find the limit cycle
+
+param: function - f - the function to be solved
+param: float - y0 - the initial condition
+param: float - t0 - the initial time
+
+returns:
+array - Y - the solution values and the
+array - t  - time for these solutions
+
+'''
+
+def shooting(f, y0, t0,tol=1e-3):
+    # initialize the solution
+    guess = [y0]
+    t = [t0]
+
+    # set the initial iteration
+    iteration = 0
+
+    # set the initial error
+    error = 1
+
+    # loop until we reach the tolerance
+    while error > tol:
+        # solve the ODE with the guess
+        Y, t = solve_to(f, guess, t0, 100,tol,  'RK4' )
+
+        # calculate the error
+        error = abs(Y[-1][0] - Y[0][0])
+
+        # update the guess
+        guess = guess - error
+
+        # update the iteration
+        iteration += 1
+
+    return Y, t
+
