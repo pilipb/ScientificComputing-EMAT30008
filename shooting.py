40ddea9f99a8d12c384db6abb992fcbb057433a4
pilipb <philipblecher@hotmail.com>
2023-02-12 21:35:03 +0000

    Working shooting function

--- shooting.py
+++ shooting.py
@@ -24,7 +24,7 @@ def shooting(f, y0, method):
     t0 = t[0]
     t1 = 100
 
-    guess = [1, y0]
+    guess = 0.01
 
     delta_t = 0.01
     step = 0.01
@@ -38,30 +38,29 @@ def shooting(f, y0, method):
 
 
     # solve ode at y0 guess
-    Y , t = solve_to(f, guess, t0, t1, delta_t, method)
+    Y , t = solve_to(f, [1,guess], t0, t1, delta_t, method)
     Y = np.array(Y)
 
     # find dy/dt 
-    dydt = lambda ode: ode[1]
+    def dydt(x,y,b):
+        return b*y*(1- (y/x))
 
     x,y = Y[:,0], Y[:,1]
-    dy = dydt(f([x,y],t))
-
-    print('Initial guess: ', y0, 'dy/dx at t = 100 (arbitrary): ', dy[-1])
-    plt.plot(t, y, label='guess = %.2f' %y0)
+    dy = dydt(x,y,b)
+    print('Initial guess: ', guess, 'dy/dx at t = 100 (arbitrary): ', dy[-1])
+    plt.plot(t, y, label='guess = %.2f' %guess)
 
     # the goal is to make the gradient dy/dx = 0 at t = 100
     # we can do this by shooting the solution until the gradient is zero
 
     while np.round(dy[-1],3) != 0:
-        y0 += step * (0 - dy[-1]) 
-        guess = [1, y0]
-        Y, t = solve_to(ode, guess, 0, 100, 0.01, 'RK4')
+        guess += step * (0 - dy[-1]) 
+        Y, t = solve_to(ode, [1,guess], 0, 100, 0.01, 'RK4')
         Y = np.array(Y)
         x , y = Y[:,0], Y[:,1]
-        dy = dydt(f(x,y), t)
-        print('New guess: ', y0, 'dy/dx at t = 100 (arbitrary): ', dy[-1])
-        plt.plot(t, y, label='guess = %.2f' %(y0))
+        dy = dydt(x,y,b)
+        print('New guess: ', guess, 'dy/dx at t = 100 (arbitrary): ', dy[-1])
+        plt.plot(t, y, label='guess = %.2f' %(guess))
 
 #### TEST ####
 
@@ -74,5 +73,6 @@ def ode(Y, t, args = (a, b, d)):
     return np.array([x*(1-x) - (a*x*y)/(d+x) , b*y*(1- (y/x))])
 
 shooting(ode, 0.1,'RK4')
+plt.show()
 
 
