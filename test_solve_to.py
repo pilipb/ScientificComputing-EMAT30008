e14011ef8816942e296fefc2a5de93fe34571786
pilipb <xq19351@bristol.ac.uk>
2023-02-24 11:36:28 +0000

    tests passed

--- test_solve_to.py
+++ test_solve_to.py
@@ -8,12 +8,11 @@ def func(x, t):
     return x
 
 def test_solve_to_step():
-    x1, t1 = solve_to(func, 0,0,step,delta_t, method='euler')
-    assert t1 == step
-
+    x1, t1 = solve_to(func, 0,0,step,delta_t, method='Euler')
+    assert round(t1[-1] -  step, 1) ==0
 
 def test_solve_to_output():
-    x1, t1 = solve_to(func, 0,0,step,delta_t, method='euler')
+    x1, t1 = solve_to(func, 0,0,step,delta_t, method='Euler')
     assert type(x1) == np.ndarray
     assert type(t1) == np.ndarray
 
