66098294a7d4f22b07e5d8412c1eaef9bba7c6b5
pilipb <xq19351@bristol.ac.uk>
2023-02-28 22:29:46 +0400

    notes added

--- unit_test.py
+++ unit_test.py
@@ -4,6 +4,23 @@ from shooting import shooting
 import unittest 
 import numpy as np
 
+'''
+Test the user interface functions, solve_to and shooting, for incorrect inputs so that
+the user is notified of which inputs are incorrect.
+
+Test all the possible inputs for the solve_to function:
+    - f - function: the function to be integrated (with inputs (Y,t)) in first order form of n dimensions
+    - y0 - array: the initial value of the solution
+    - t0 - float: the initial value of time
+    - t1 - float: the end time
+    - delta_t - float: the step size
+    - method - string: the method to be used to solve the ODE (Euler, RK4, Heun)
+
+and the same for the shooting function
+
+
+'''
+
 class solver_test(unittest.TestCase):
     def test_euler(self):
         step = 3
