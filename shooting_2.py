119d7e135efcfa93652653a28905f0ef4e12003d
pilipb <xq19351@bristol.ac.uk>
2023-04-20 15:48:15 +0100

    basic shooting implemented from scratch

--- shooting_2.py
+++ shooting_2.py
@@ -40,19 +40,128 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as scipy
 import math
+from solve_to import solve_to
 
-def ode(t, Y, args):
+# consider the ode:
+# mx¨ + cx˙ + kx = gamma sin(ωt)
+
+# T = 2π/omega
+
+# in first order form:
+# u1' = u2
+# u2' = 1/m (-c u2 - k u1 + gamma sin(ωt))
+
+# the time solution is:
+
+# u(t) = [u1(t), u2(t)] = F(t, u0) (where F is the integration of u with the initial conditions u0, up to time t)
+
+# the limit cycle is when u0 - u(T) = 0 for some u0
+
+# u0 - F(T, u0) = 0
+
+# ie: solving the root for G(u0) = 0, where G(u0) = u0 - F(T, u0)
+
+# solve by passing G(u0) to fsolve
+
+# IMPLEMENTATION:
+
+# define the function to be integrated
+def f(t, Y, *args):
+    # unpack the arguments
+    # print(args)
+    m, c, k, gamma, omega = args[0]
+
+    # unpack the variables
     u1, u2 = Y
-    D = args[0]
-    return np.array([u2, 0])
 
-# [u1(0) - A, u1(1) - B, phase(T) - phase(0)] = 0
+    # define the derivatives
+    u1p = u2
+    u2p = 1/m*(-c*u2 - k*u1 + gamma*np.sin(omega*t))
 
-# u(0) = [A, alpha = 0] = [u1(0), u2(0)]
-# u(1) = u1(1), u2(1) = [B, _ ]
+    # return the derivatives
+    return np.array([u1p, u2p])
+ 
+
+# define the function to be solved for the initial conditions and period that satisfy the boundary conditions
+def G(u0, *args):
+    # unpack the arguments
+    m, c, k, gamma, omega = args
+
+    # unpack the initial conditions
+    u10, u20 = u0
+
+    # define the initial conditions
+    u0 = np.array([u10, u20])
+
+    # define the time interval
+    t0 = 0
+    T = 2*np.pi/omega
+    t = np.linspace(t0, T, 1000)
+
+    # solve the ode
+    Y, _ = solve_to(f, u0, t0, T, 0.01, 'RK4', args=args)
+
+    # define the boundary conditions
+    u1T = Y[-1,0]
+    u2T = Y[-1,1]
+
+    # # define the phase condition
+    # u1p = f(t, Y, args)[:,0]
+    # phase = u1p[-1]
+
+    # return the boundary conditions and phase condition
+    return np.array([u1T - u10, u2T - u20])#, phase
+
+# run the code for an example
+
+# define the parameters
+m = 1
+c = 1
+k = 1
+gamma = 1
+omega = 1
+
+# define the arguments
+args = (m, c, k, gamma, omega)
+
+# define the initial conditions
+u10 = 0
+u20 = 0
+u0 = np.array([u10, u20])
+
+# solve for the initial conditions and period that satisfy the boundary conditions
+u0 = scipy.fsolve(G, u0, args=args, full_output=True)
+
+print(u0)
+# unpack the initial conditions
+u10, u20 = u0[0]
+
+# define the initial conditions
+u0 = np.array([u10, u20])
+
+# define the time interval
+t0 = 0
+T = 2*np.pi/omega
+# t = np.linspace(t0, T, 1000)
+
+# solve the ode
+Y, t = solve_to(f, u0, t0, T, 0.01, 'RK4', args=args)
+
+# unpack the solution
+u1 = Y[:,0]
+u2 = Y[:,1]
+
+# plot the solution
+plt.plot(t, u1, label='u1')
+plt.plot(t, u2, label='u2')
+plt.legend()
+plt.show()
+
+# for autonomous systems, the phase condition is dx/dt(0) = 0
 
 
-def shooting(f, y0, T, args = None):
+
+
 
 
 
