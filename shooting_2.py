e6a1ccfca19800bbd302b086366e02956999a2af
pilipb <xq19351@bristol.ac.uk>
2023-04-20 15:14:25 +0100

    set up?

--- shooting_2.py
+++ shooting_2.py
@@ -14,21 +14,48 @@ u2_t = 0
 
 with boundary conditions u1(0) = A and u1(1) = B and u2(0) = alpha (unknown)
 
-we can solve this system using the shooting method:
+we can solve this system using the shooting method (and for the limit cycle):
 
-f(u) = [u1(0) - A, u1(1) - B] = 0
+f(u) = [u1(0) - A, u1(1) - B, phase(T) - phase(0)] = 0
+
+where phase(T) is the phase condition of the solution at time T and phase(0) is the phase condition of the solution at time 0
+The phase condition can be u_x 
 
 u1(1) is found by numerical integration
 
 start with a guess u(0) = [A, alpha=0]
 
-use newton's method to iterate to a solution
+use newton's method to iterate to a solution:
 
-first iteration:
-u1(1) = u1(0) + u2(0) * dt
+integrate the system from 0 to 1 with initial conditions u(0) = [A, alpha]
 
+update alpha = u1(1) - B
 
-
-u
+repeat until f(u) = 0
 
 '''
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.optimize as scipy
+import math
+
+def ode(t, Y, args):
+    u1, u2 = Y
+    D = args[0]
+    return np.array([u2, 0])
+
+# [u1(0) - A, u1(1) - B, phase(T) - phase(0)] = 0
+
+# u(0) = [A, alpha = 0] = [u1(0), u2(0)]
+# u(1) = u1(1), u2(1) = [B, _ ]
+
+
+def shooting(f, y0, T, args = None):
+
+
+
+
+
+
