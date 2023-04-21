ee8706a1b842a9a333bb5b268055f4c075c5be5a
pilipb <xq19351@bristol.ac.uk>
2023-04-21 14:50:03 +0100

    separate continuation into natural and pal

--- continuation.py
+++ continuation.py
@@ -1,6 +1,7 @@
 from solve_to import *
 from solvers import *
-from shooting_2 import shooting
+from shooting import shooting
+from test_shoot import test_shooting
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as scipy
@@ -25,6 +26,8 @@ def nat_continuation(f, u0, plim, T, *args, plot = True):
             the initial guess for the period of the solution
     args : tuple
             the constant arguments for the function f
+    plot : bool
+            whether or not to plot the solutions
 
 
     Returns
@@ -55,7 +58,7 @@ def nat_continuation(f, u0, plim, T, *args, plot = True):
         Y1, T1 = shooting(f, u0, T0, p0)
 
         # plot the solution
-        y,t = solve_to(hopf_bifurcation, u0, 0, T1, 0.01, 'RK4', (p0,))
+        y,t = solve_to(hopf_bifurcation, u0, 0, T1, 0.01, 'RK4', p0)
 
         if plot == True:
             # on one plot, plot the y1 vs y2 phase plot
@@ -81,10 +84,95 @@ def nat_continuation(f, u0, plim, T, *args, plot = True):
     return T_list, p_list
 
 
-# # define a pseudo arc length continuation function
-# def pal_continuation(f, u0, plim, T, *args, plot = True):
+# define a pseudo arc length continuation function
+def pal_continuation(f, u0, u1, T, *args, plot = True):
+    '''
+    Pseudo arc length continuation method to find the solution to the ODE f for varying parameter value p
 
-#     pass
+    Parameters
+    ----------------------------
+    f : function
+            the function to be integrated (with inputs (t, Y, *args)) in first order form of n dimensions
+    u0 : array [y1, y2, ... , p0]
+            the initial conditions guess for the integration with the initial parameter value
+    u1 : array [y1, y2, ... , p1]
+            a second initial conditions guess for the integration with the second parameter value
+    T : float
+            the initial guess for the period of the solution
+    args : tuple
+            the constant arguments for the function f
+    plot : bool
+            whether or not to plot the solutions
+
+
+    Returns
+    ----------------------------
+    T_list : list
+            a list of the periods for the solutions found for each parameter value
+    p_list : list
+            a list of the parameter values used to find the solutions
+
+    '''
+
+    '''
+    Method:
+
+    Start with shooting solutions for u0 and u1
+
+    Find the secant vector from u0 to u1
+
+    Create estimate of u2 : u2 = u1 + step*secant
+
+    Find the shooting solution for u2 but with psuedo arc length continuation equation added to the f(u) = 0 equation
+
+    pal eq: (true_u2 - u2) dot sec = 0
+
+    '''
+    # store the solutions
+    T_list = []
+
+    if plot == True:
+        # plot the solutions on two plots
+        fig, ax = plt.subplots(1, 2)
+
+    # plist
+    p_list = np.arange(plim[0], plim[1], 0.1)
+
+
+    while p0 < plim[1]:
+
+        # find the shooting solution for the initial parameter value
+        T0 = T
+
+        Y1, T1 = test_shooting(f, u0, T0, p0)
+
+        # plot the solution
+        y,t = solve_to(hopf_bifurcation, u0, 0, T1, 0.01, 'RK4', p0)
+
+        if plot == True:
+            # on one plot, plot the y1 vs y2 phase plot
+            ax[0].plot(y[:,0], y[:,1])
+        
+        # store the solution
+        T_list.append(T1)
+
+    if plot == True:
+        # plot the period vs the parameter value
+        ax[1].plot(np.arange(plim[0], plim[1], 0.1), T_list)
+
+        # show and label the plots
+        ax[0].set_xlabel('y1')
+        ax[0].set_ylabel('y2')
+        ax[0].set_title('Phase Plot')
+        ax[1].set_xlabel('p')
+        ax[1].set_ylabel('T')
+        ax[1].set_title('Period vs Parameter')
+
+        plt.show()
+
+    return T_list, p_list
+
+
 
 
 
@@ -97,8 +185,12 @@ def nat_continuation(f, u0, plim, T, *args, plot = True):
 ### TEST
 
 # define the function to be integrated
-def hopf_bifurcation(t, Y, *args):
-    b = args[0][0]
+def hopf_bifurcation(t, Y, args):
+    try:
+        b = args[0]
+    except:
+        b = args
+
     x, y = Y
     dxdt = b*x - y - x*(x**2 + y**2)
     dydt = x + b*y - y*(x**2 + y**2)
