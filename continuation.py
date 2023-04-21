054cb2ab99ca8286ace0f219cdc4e1d3509346ab
pilipb <xq19351@bristol.ac.uk>
2023-04-21 17:25:02 +0100

    setting up natural continuation for multiple variables

--- continuation.py
+++ continuation.py
@@ -20,8 +20,6 @@ results = continuation(myode,  # the ODE to use
 
 # natural continuation will find the roots of the equation, then increment c and find the roots again
 
-def cubic(x, c):
-    return x**3 - x + c
 
 def nat_continuation(ode, x0, p0 = 0, vary_p =0, step = 0.1, max_steps = 100, discret=lambda x: x, solver=scipy.optimize.fsolve):
 
@@ -29,32 +27,46 @@ def nat_continuation(ode, x0, p0 = 0, vary_p =0, step = 0.1, max_steps = 100, di
     X = []
     C = []
 
-    c0 = c_lim[0]
-
-    # find initial solution F(u) = 0
-    sol = scipy.optimize.fsolve(ode, x0, args = (c0))
+    # discretise the ode
+    '''
+    if the function is an algebraic equation, then the discretisation is just the function itself
+    if the function is a differential equation, then the discretisation is calling shooting method
+    to make the F(x) = 0 that will be solved by the solver
+    
+    '''
+    
+    
+    sol = scipy.optimize.fsolve(ode, x0, args = (p0))
 
     # append the solution
     X.append(sol)
-    C.append(c0)
+    C.append(p0)
 
+    num_steps = 0
     # loop with incrementing c until reaching the limit
-    while c0 < c_lim[1]:
-        c0 += 0.01
-        sol = scipy.optimize.fsolve(ode, sol, args = (c0))
+    while num_steps < max_steps:
+        p0 += step
+        sol = scipy.optimize.fsolve(ode, sol, args = (p0))
         X.append(sol)
-        C.append(c0)
+        C.append(p0)
+        num_steps += 1
 
     return X, C
+
+
+# define the cubic equation
+def cubic(x, *args):
+    
+    c = args
+    return x**3 - x + c
 
 # define the initial conditions
 x0 = 1
 
 # define the limit of c
-c_lim = [-2, 2]
 
 # solve the system of equations for the initial conditions [x0, y0, ... ] and period T that satisfy the boundary conditions
-X, C = nat_continuation(cubic, x0, c_lim)
+X, C = nat_continuation(cubic, x0, -2)
 
 # plot the solution
 plt.plot(C, X)
