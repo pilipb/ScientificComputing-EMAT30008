cdc8c263b67b8aac897c838add947d133d142161
pilipb <xq19351@bristol.ac.uk>
2023-04-21 17:04:30 +0100

    structure for full continuation

--- continuation.py
+++ continuation.py
@@ -2,6 +2,18 @@ import scipy.optimize
 import numpy as np
 import matplotlib.pyplot as plt
 
+'''
+results = continuation(myode,  # the ODE to use
+    x0,  # the initial state
+    par0,  # the initial parameters
+    vary_par=0,  # the parameter to vary
+    step_size=0.1,  # the size of the steps to take
+    max_steps=100,  # the number of steps to take
+    discretisation=shooting,  # the discretisation to use
+    solver=scipy.optimize.fsolve)  # the solver to use
+    
+    '''
+
 # have an algebraic cubic equation
 
 # x^3 - x + c = 0
@@ -11,7 +23,7 @@ import matplotlib.pyplot as plt
 def cubic(x, c):
     return x**3 - x + c
 
-def natural_continuation(f, x0, c_lim, args = None):
+def nat_continuation(ode, x0, p0 = 0, vary_p =0, step = 0.1, max_steps = 100, discret=lambda x: x, solver=scipy.optimize.fsolve):
 
     # initialize the solution
     X = []
@@ -20,7 +32,7 @@ def natural_continuation(f, x0, c_lim, args = None):
     c0 = c_lim[0]
 
     # find initial solution F(u) = 0
-    sol = scipy.optimize.fsolve(f, x0, args = (c0))
+    sol = scipy.optimize.fsolve(ode, x0, args = (c0))
 
     # append the solution
     X.append(sol)
@@ -29,7 +41,7 @@ def natural_continuation(f, x0, c_lim, args = None):
     # loop with incrementing c until reaching the limit
     while c0 < c_lim[1]:
         c0 += 0.01
-        sol = scipy.optimize.fsolve(f, sol, args = (c0))
+        sol = scipy.optimize.fsolve(ode, sol, args = (c0))
         X.append(sol)
         C.append(c0)
 
@@ -42,7 +54,7 @@ x0 = 1
 c_lim = [-2, 2]
 
 # solve the system of equations for the initial conditions [x0, y0, ... ] and period T that satisfy the boundary conditions
-X, C = natural_continuation(cubic, x0, c_lim)
+X, C = nat_continuation(cubic, x0, c_lim)
 
 # plot the solution
 plt.plot(C, X)
