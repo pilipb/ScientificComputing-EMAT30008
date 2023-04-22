841aa06cde554b60a4cb197e7f5b967102ef34ab
pilipb <xq19351@bristol.ac.uk>
2023-04-22 13:04:46 +0100

    shooting now as part of Discretisation class including linear discretisation

--- shooting.py
+++ shooting.py
@@ -6,64 +6,78 @@ import matplotlib.pyplot as plt
 import scipy
 import math
 
-
-def shooting_setup(f, y0, T= 0, args = None):
-
-    '''
-    Implementing a numerical shooting method to solve an ODE to find a periodic solution
-    This method will solve the BVP ODE using root finding method to find the limit cycle, using scipy.optimize.fsolve to
-    find the initial conditions and period that satisfy the boundary conditions.
-
-    parameters:
-    ----------------------------
-    f - function: the function to be integrated (with inputs (Y,t)) in first order form of n dimensions
-    y0 - array: the initial value of the solution
-    T - float: an initial guess for the period of the solution
-    args - array: the arguments for the function f
-
-    returns:
-    ----------------------------
-    sol - array: the initial conditions that cause the solution to be periodic: sol = [x0, y0, ... , T]
-
-    '''
-
-    # define the function that will be solved for the initial conditions and period
-    def fun(initial_vals,p):
+class Discretisation():
+    def __init__(self):
         '''
-        Function F(u) = 0 that will be solved for the initial conditions and period
-        Parameters:
-        ----------------------------
-        initial_vals - array: the initial conditions and period guess: initial_vals = [x0, y0, ... , T]
+        Class to discretise the ODEs
+        
+        '''
+        pass
 
-        Returns:
+    def shooting_setup(self, f, y0, T= 0, args = None):
+
+        '''
+        Implementing a numerical shooting method to solve an ODE to find a periodic solution
+        This method will solve the BVP ODE using root finding method to find the limit cycle, using scipy.optimize.fsolve to
+        find the initial conditions and period that satisfy the boundary conditions.
+
+        parameters:
         ----------------------------
-        row - array: the boundary conditions that must be satisfied for the solution to be periodic: 
-                    row = [x(T) - x0, y(T) - y0, ... , dx/dt(0) = 0]
+        f - function: the function to be integrated (with inputs (Y,t)) in first order form of n dimensions
+        y0 - array: the initial value of the solution
+        T - float: an initial guess for the period of the solution
+        args - array: the arguments for the function f
+
+        returns:
+        ----------------------------
+        sol - array: the initial conditions that cause the solution to be periodic: sol = [x0, y0, ... , T]
 
         '''
 
-        # unpack the initial conditions and period guess
-        T = initial_vals[-1]
-        y0 = initial_vals[:-1] 
+        # define the function that will be solved for the initial conditions and period
+        def fun(initial_vals,p):
+            '''
+            Function F(u) = 0 that will be solved for the initial conditions and period
+            Parameters:
+            ----------------------------
+            initial_vals - array: the initial conditions and period guess: initial_vals = [x0, y0, ... , T]
 
-        Y , _ = solve_to(f, y0, 0, T, 0.01, 'RK4', args=args)
+            Returns:
+            ----------------------------
+            row - array: the boundary conditions that must be satisfied for the solution to be periodic: 
+                        row = [x(T) - x0, y(T) - y0, ... , dx/dt(0) = 0]
 
-        # make empty array to store the boundary conditions
-        num_dim = len(y0)
-        row = np.zeros(num_dim)
+            '''
 
-        # limit cycle condition
-        row = Y[-1,:] - y0[:]
-  
-        # phase condition
-        row = np.append(row, f(0, Y[0,:], args)[0]) # dx/dt(0) = 0 
+            # unpack the initial conditions and period guess
+            T = initial_vals[-1]
+            y0 = initial_vals[:-1] 
 
-        return row
+            Y , _ = solve_to(f, y0, 0, T, 0.01, 'RK4', args=args)
 
-    # solve the system of equations for the initial conditions [x0, y0, ... ,T] that satisfy the boundary conditions
-    u0 = np.append(y0, T)
+            # make empty array to store the boundary conditions
+            num_dim = len(y0)
+            row = np.zeros(num_dim)
 
-    return fun
+            # limit cycle condition
+            row = Y[-1,:] - y0[:]
+    
+            # phase condition
+            row = np.append(row, f(0, Y[0,:], args)[0]) # dx/dt(0) = 0 
+
+            return row
+
+        # solve the system of equations for the initial conditions [x0, y0, ... ,T] that satisfy the boundary conditions
+        u0 = np.append(y0, T)
+
+        return fun
+    
+    def linear(self, x, x0, T, args):
+        '''
+        Linear discretisation of the ODE (x => x)
+        
+        '''
+        return x 
 
 
 def shooting_solve(fun, u0):
@@ -137,8 +151,9 @@ if __name__ == '__main__':
     Y0 = [0.1,0.1]
     T = 10
     
+    discret = Discretisation()
     # solve the ode using the shooting method
-    fun = shooting_setup(hopf, Y0, T=T, args=b)
+    fun = discret.shooting_setup(hopf, Y0, T=T, args=b)
     u0 = np.append(Y0, T)
     u0, T0 = shooting_solve(fun, u0)
 
